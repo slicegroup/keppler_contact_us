17e36a1548f1b5beb2279ef3936764e760286770
Gabriel Morillo <gbrlmrllo@gmail.com>
2016-05-25 21:52:37 -0430

    adapted to keppler new version

--- app/models/keppler_contact_us/message.rb
+++ app/models/keppler_contact_us/message.rb
@@ -1,28 +1,19 @@
-#Generado por keppler
-require 'elasticsearch/model'
 module KepplerContactUs
   class Message < ActiveRecord::Base
-    include Elasticsearch::Model
-    include Elasticsearch::Model::Callbacks
+    include ElasticSearchable
+    include ActivityHistory
     validates_presence_of :name, :email
-    
-    after_commit on: [:update] do
-      __elasticsearch__.index_document
-    end
-
-    def self.searching(query)
-      if query
-        self.search(self.query query).records.order(id: :desc)
-      else
-        self.order(id: :desc)
-      end
-    end
 
     def self.query(query)
-      { query: { multi_match:  { query: query, fields: [:name, :subject, :create_at, :email, :read] , operator: :and }  }, sort: { id: "desc" }, size: self.count }
+      { query: {
+        multi_match:  {
+          query: query,
+          fields: [:name, :subject, :create_at, :email, :read],
+          operator: :and
+        }
+      }, sort: { id: "desc" }, size: self.count }
     end
 
-    #armar indexado de elasticserch
     def as_indexed_json(options={})
       {
         id: self.id.to_s,
@@ -30,11 +21,8 @@ module KepplerContactUs
         subject:  self.subject,
         email:  self.email,
         content:  self.content,
-        read:  self.read ? "si--leidos" : "no--leidos",
         created_at:  self.created_at.to_s,
       }.as_json
     end
-
   end
-  #Message.import
 end
--- lib/keppler_contact_us/engine.rb
+++ lib/keppler_contact_us/engine.rb
@@ -4,5 +4,9 @@ module KepplerContactUs
     config.generators do |g|
         g.template_engine :haml
     end
+
+    config.to_prepare do
+      ApplicationController.helper(ApplicationHelper)
+    end
   end
 end
