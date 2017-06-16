02c3f140a3662805fc5ce74b57e760e3dcdbb28b
Hugojose12 <hugo200440@gmail.com>
2017-06-16 09:52:05 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ KepplerContactUs::Engine.routes.draw do
     resources :message_settings, only: [] do
       collection do
         get '/', to: 'message_settings#edit', as: 'edit'
+        post '/', to: 'message_settings#new', as: 'new'
         patch '/', to: 'message_settings#update', as: 'update'
       end
     end
